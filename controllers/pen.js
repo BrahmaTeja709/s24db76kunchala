fbf1756a568d034d29aa866100375b98f87d1d86
BrahmaTeja709 <156278821+BrahmaTeja709@users.noreply.github.com>
2024-04-05 15:57:39 -0500

    v

--- controllers/pen.js
+++ controllers/pen.js
@@ -24,10 +24,18 @@ exports.pen_view_all_Page = async function(req, res) {
     }
     };
 // for a specific pen.
-exports.pen_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: pen detail: ' + req.params.id);
-};
-// Handle pen create on POST.
+// for a specific Costume.
+exports.pen_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await pen.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    // Handle pen create on POST.
 // Handle Costume create on POST.
 exports.pen_create_post = async function(req, res) {
 console.log(req.body)
@@ -54,6 +62,23 @@ exports.pen_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: pen delete DELETE ' + req.params.id);
 };
 // Handle pen update form on PUT.
-exports.pen_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: pen update PUT' + req.params.id);
+// Handle Costume update form on PUT.
+exports.pen_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await pen.findById( req.params.id)
+// Do updates of properties
+if(req.body.pen_type)
+toUpdate.pen_type = req.body.pen_type;
+if(req.body.pen) toUpdate.pen = req.body.pen;
+if(req.body.pen) toUpdate.pen = req.body.pen;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
